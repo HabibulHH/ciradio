d6e8df408d8d0de6169f07ce0325bfeff7eb72b0
HabibulHH <habibul.hasan.hira@gmail.com>
2022-01-21 21:35:45 +0600

    Cover image css added with items details design

--- src/components/details.tsx
+++ src/components/details.tsx
@@ -5,6 +5,7 @@ function RadioItemDetails() {
      <div className="item-details">
       <div><img src='/minus.png'/></div>
       <div className='photo'>
+        <img className='radio-cover' src='/radio.png'/>
       </div>
       <div><img src='/plus.png'/></div>
     </div>
