8b569050316868383ed8a4bf84462d1632b22026
HabibulHH <habibul.hasan.hira@gmail.com>
2022-01-21 22:04:19 +0600

    state wise data rendered

--- src/components/radioItem.tsx
+++ src/components/radioItem.tsx
@@ -1,11 +1,17 @@
+import { useState } from 'react';
 import '../App.css';
 import RadioItemDetails from './details';
 
 function RadioItem() {
+  const [clicked,setClick] = useState(false);
+
+  const handleClick = ()=>{
+    setClick(!clicked);
+  } 
   return (
      <>
-     <RadioItemDetails/>
-     <div className="station-item">
+     {clicked?<RadioItemDetails/>:""}
+     <div onClick={()=>{handleClick()}} className="station-item">
       <div className='station-name'>Station name</div>
       <div className='station-number'>Station freequency</div>
     </div>
