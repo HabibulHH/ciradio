2cf645cc15a1c040ec0ea8a9aa8c60da5e90d1a7
HabibulHH <habibul.hasan.hira@gmail.com>
2022-01-23 12:10:55 +0600

    Added accordion for radio details

--- src/components/radioItem.tsx
+++ src/components/radioItem.tsx
@@ -1,23 +1,62 @@
-import { useState } from 'react';
-import '../App.css';
+import React from 'react';
+
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemHeading,
+    AccordionItemButton,
+    AccordionItemPanel,
+} from 'react-accessible-accordion';
+
+// Demo styles, see 'Styles' section below for some notes on use.
+
 import RadioItemDetails from './details';
 
-function RadioItem() {
-  const [clicked,setClick] = useState(false);
-
-  const handleClick = ()=>{
-    setClick(!clicked);
-  } 
-  return (
-     <>
-     {clicked?<RadioItemDetails/>:""}
-     <div onClick={()=>{handleClick()}} className="station-item">
-      <div className='station-name'>Station name</div>
-      <div className='station-number'>Station freequency</div>
-    </div>
-
-    </>
-  );
+export default function Example() {
+    return (
+        <Accordion allowMultipleExpanded ={false}>
+            <AccordionItem>
+            <AccordionItemPanel>
+                <RadioItemDetails/>
+              </AccordionItemPanel>
+                <AccordionItemHeading>
+                    <AccordionItemButton>
+                    <div className="station-item">
+                  <div className='station-name'>Radio amar</div>
+                  <div className='station-number'>89.50</div>
+                </div>
+                    </AccordionItemButton>
+                </AccordionItemHeading>
+            </AccordionItem>
+            <hr />
+            <AccordionItem>
+            <AccordionItemPanel>
+                <RadioItemDetails/>
+              </AccordionItemPanel>
+                <AccordionItemHeading>
+                    <AccordionItemButton>
+                    <div className="station-item">
+                  <div className='station-name'>Radio foorti</div>
+                  <div className='station-number'>330</div>
+                </div>
+                    </AccordionItemButton>
+                </AccordionItemHeading>
+            </AccordionItem>
+            <hr />
+            <AccordionItem>
+            <AccordionItemPanel>
+                <RadioItemDetails/>
+              </AccordionItemPanel>
+                <AccordionItemHeading>
+                    <AccordionItemButton>
+                    <div className="station-item">
+                  <div className='station-name'>Radio Today</div>
+                  <div className='station-number'>100.45</div>
+                </div>
+                    </AccordionItemButton>
+                </AccordionItemHeading>
+            </AccordionItem>
+        </Accordion>
+    );
 }
 
-export default RadioItem;
--- src/components/radioList.tsx
+++ src/components/radioList.tsx
@@ -1,22 +1,29 @@
+import { useState } from 'react';
 import '../App.css';
 import RadioItem from './radioItem';
 
 function RadioList() {
+  const [v, setV] = useState(false);
+  const handleClick = ()=>{
+    console.log('hira hasan');
+    
+  }
   return (
      <div className="station-container">
-       <RadioItem  />
-       <hr />
        <RadioItem/>
        <hr />
-       <RadioItem/>
+       {/* <RadioItem /> */}
+       {/* <RadioItem data ={2} />
        <hr />
-       <RadioItem/>
+       <RadioItem data ={3}/>
+       <hr />
+       <RadioItem data ={4} />
         <hr />
-       <RadioItem/>  <hr />
-       <RadioItem/>  <hr />
-       <RadioItem/>  <hr />
-       <RadioItem/>  <hr />
-       <RadioItem/>
+       <RadioItem data ={5} />  <hr />
+       <RadioItem data ={6} />  <hr />
+       <RadioItem data ={7}/>  <hr />
+       <RadioItem data ={8} />  <hr />
+       <RadioItem data ={9}/> */}
      </div>
   );
 }
